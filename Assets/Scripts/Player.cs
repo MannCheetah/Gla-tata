05e3ee1788d0de7150316f0f7b6499238094cb94
Michael <michaeldev720@gmail.com>
2023-08-06 00:12:24 -0700

    made movement for player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed = 4.20f;
+    public CharacterController myController;
+
     // Start is called before the first frame update
     void Start()
     {
         print("Ello Mofo!");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myController.Move(Vector3.up * speed);
+            print("Giggity Hop!");
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            myController.Move(Vector3.left * speed);
+            print("moved left!");
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            myController.Move(Vector3.forward * speed);
+            print("moved forward!");
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            myController.Move(Vector3.right * speed);
+            print("moved right!");
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            myController.Move(Vector3.back * speed);
+            print("Giggity Bottom");
+        }
+
     }
 }
