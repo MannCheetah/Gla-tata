19c456c6cc99ee3e152e57a882ae0dfa1232dee3
Michael <michaeldev720@gmail.com>
2023-08-05 21:43:21 -0700

    made playground and player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Ello Mofo!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
